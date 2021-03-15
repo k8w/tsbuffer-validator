daadcc7cfa57755389b80e9abb2d0b039e580094
King Wang <me@k8w.io>
2021-03-15 22:44:37 +0800

    prune test

--- test/src/TSBufferValidator/prune.test.ts
+++ test/src/TSBufferValidator/prune.test.ts
@@ -1,66 +1,55 @@
-// import * as assert from 'assert';
-// import { TSBufferProto } from 'tsbuffer-schema';
-// import { TSBufferValidator } from '../../../src/TSBufferValidator';
+import * as assert from 'assert';
+import { TSBufferProto } from 'tsbuffer-schema';
+import { TSBufferValidator } from '../../../src/TSBufferValidator';
 
-// const proto: TSBufferProto = require('../../genTestSchemas/output');
-// let validator = new TSBufferValidator(proto);
+const proto: TSBufferProto = require('../../genTestSchemas/output');
+let validator = new TSBufferValidator(proto);
 
-// describe('prune', function () {
-//     it('basic interface', function () {
-//         assert.deepStrictEqual(validator.prune({ a: 'aaa', b: 1, c: 'asdg' }, 'interface1/Interface1').output, { a: 'aaa', b: 1 });
+describe('prune', function () {
+    it('basic interface', function () {
+        assert.deepStrictEqual(validator.prune({ a: 'aaa', b: 1, c: 'asdg' }, 'interface1/Interface1'), { a: 'aaa', b: 1 });
+        assert.deepStrictEqual(validator.prune({ asdg: 234, a: 'aaa', asdgasdg: 'asdgasdg,', b: 1, c: 'asdg' }, 'interface1/Interface1'), { a: 'aaa', b: 1 });
+    })
 
-//         let res1 = validator.prune({ a: 'aaa', c: 'asdg' }, 'interface1/Interface1');
-//         assert.strictEqual(res1.isSucc, false);
-//         assert.strictEqual(res1.output, undefined);
+    it('index signature', function () {
+        assert.deepStrictEqual(validator.prune({
+            name: 'asdf',
+            sex: 'm',
+            aaa: 'asdgasdg',
+            bbb: 'asdgasdg'
+        }, 'interface1/Interface2_1'), {
+            name: 'asdf',
+            sex: 'm',
+            aaa: 'asdgasdg',
+            bbb: 'asdgasdg'
+        });
+    })
 
-//         assert.deepStrictEqual(validator.prune(123, 'interface1/Interface1').output, undefined);
-//     })
+    it('logic', function () {
+        assert.deepStrictEqual(validator.prune({
+            a: 'sss',
+            b: 'fff',
+            c: 'asdf'
+        }, 'logic/AB'), {
+            a: 'sss',
+            b: 'fff'
+        });
 
-//     it('index signature', function () {
-//         assert.deepStrictEqual(validator.prune({
-//             name: 'asdf',
-//             sex: 'm',
-//             aaa: 'asdgasdg',
-//             bbb: 'asdgasdg'
-//         }, 'interface1/Interface2_1').output, {
-//             name: 'asdf',
-//             sex: 'm',
-//             aaa: 'asdgasdg',
-//             bbb: 'asdgasdg'
-//         });
-//     })
-
-//     it('logic', function () {
-//         assert.deepStrictEqual(validator.prune({
-//             a: 'sss',
-//             b: 'fff',
-//             c: 'asdf'
-//         }, 'logic/AB').output, {
-//             a: 'sss',
-//             b: 'fff'
-//         });
-
-//         assert.deepStrictEqual(validator.prune({
-//             a: 'sss',
-//             b: 'fff',
-//             c: 'asdf'
-//         }, 'logic/CD').output, undefined);
-//         validator.options.skipExcessCheck = true;
-//         assert.deepStrictEqual(validator.prune({
-//             a: 'sss',
-//             b: 'fff',
-//             c: 'asdf'
-//         }, 'logic/CD').output, {
-//             c: 'asdf'
-//         });
-//         assert.deepStrictEqual(validator.prune({
-//             a: 'sss',
-//             b: 'fff',
-//             c: 'asdf',
-//             d: 'ffd',
-//         }, 'logic/CD').output, {
-//             c: 'asdf',
-//             d: 'ffd'
-//         });
-//     })
-// })
+        assert.deepStrictEqual(validator.prune({
+            a: 'sss',
+            b: 'fff',
+            c: 'asdf'
+        }, 'logic/CD'), {
+            c: 'asdf'
+        });
+        assert.deepStrictEqual(validator.prune({
+            a: 'sss',
+            b: 'fff',
+            c: 'asdf',
+            d: 'ffd',
+        }, 'logic/CD'), {
+            c: 'asdf',
+            d: 'ffd'
+        });
+    })
+})
